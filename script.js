6cddb0d9d25f549f8951b6f59b33c2f7ac5f3751
Joshua Christianson <macgyverjunk@yahoo.com>
2022-04-18 19:04:50 -0500

    a little clean up

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 let generateBtn = document.querySelector("#generate");
 let result = []
 let generatedPasswordLength; 
-// Write password to the #password input
+
 function writePassword() { 
-  let passwordLength = generatedPassLength()
-  console.log(passwordLength);
   generatedLowercase()
   generatedUppercase()
   generatedNumber()
   generatedSymbol()
+  let passwordLength = generatedPassLength()
+  console.log(passwordLength);
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
   result = [] 
 }
+
 generateBtn.addEventListener("click", writePassword);
 function generatePassword(){
   let password = "";
@@ -22,37 +23,36 @@ function generatePassword(){
   }
   return password;
 }
-function generatedPassLength() {
-generatedPasswordLength = window.prompt("Password Length?");
-return generatedPasswordLength
-}
+
 function generatedLowercase() {
   let generatedLowerLetters = window.confirm("Would you like to use lowercase letters?")
   let lowerCase = "abcdefghijklmnopqrstuvwxyz"
   if (generatedLowerLetters === true) {
     result = result.concat(lowerCase.split(""))
-  }
-  console.log(result)
+}}
 
-}
 function generatedUppercase() {
   let generatedUpperLetters = window.confirm("Would you like to use uppercase letters?")
   let generatedUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   if (generatedUpperLetters === true) {
       result = result.concat(generatedUppercase.split(""))
-  }
-}
+}}
+
 function generatedNumber() {
   let userWantsNumbers = window.confirm("Would you like to use numbers?")
   let generatedNumber = "0123456789"
   if (userWantsNumbers === true) {
     result = result.concat(generatedNumber.split(""))
-}
-}
+}}
+
 function generatedSymbol() {
   let userWantsSymbols = window.confirm("Would you like to use symbols?")
   let generatedSymbol = "!@#$%^&*()~`_-+=}{[]:;?/>.<,"
   if (userWantsSymbols === true) {
     result = result.concat(generatedSymbol.split(""))
-}
-}
+}}
+
+function generatedPassLength() {
+  generatedPasswordLength = window.prompt("Password Length?");
+  return generatedPasswordLength
+  }
